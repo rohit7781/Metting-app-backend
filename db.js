9b406c2ad0badef8ab5e28a3a5b162d1b680a57c
Rohit Kumar <rohitkumar703314@gmail.com>
2022-01-26 00:57:44 +0530

    connecting to database

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require("dotenv").config()
+// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+
+const connectToMongo = () => {
+    mongoose.connect(process.env.mongoURI, () => {
+        console.log("Database is Connected")
+    })
+}
+
+module.exports = connectToMongo;
